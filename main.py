f661fc8979fa922b0e5f1090d01c0984c298b94d
offuttm <mason.offutt@ucdenver.edu>
2023-08-07 20:05:25 -0400

    Dtype is now torch.float32.  Matrix error is occruing because they are not the same size

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import torch
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from torch import nn
 from numpy import genfromtxt
+from sklearn.model_selection import train_test_split
+
+
 
 FILENAME = "HI-union.tsv"
 # This is a sample Python script.
@@ -39,10 +44,10 @@ def readDataNx2(name):
     print("\n")
     
     # Convert lists into np arrays
-    np_array1 = np.array(numeric_list1)
-    np_array2 = np.array(numeric_list2)
+    np_array1 = np.array(numeric_list1,dtype= np.float32)
+    np_array2 = np.array(numeric_list2,dtype= np.float32)
     
-    # onvert from np arrays to tensors
+    # convert from np arrays to tensors
     tensor1 = torch.from_numpy(np_array1)
     tensor2 = torch.from_numpy(np_array2)
     
@@ -50,11 +55,61 @@ def readDataNx2(name):
     print(f"Tensor1: {tensor1[:10]}, Tensor2: {tensor2[:10]} ")
     print(f"Tensor DTypes: {tensor1.dtype}, {tensor2.dtype}")
     print(f"Tensor Shapes: {tensor1.shape}, {tensor2.shape}")
-    
-    
-# Press the green button in the gutter to run the script.
+
+    return tensor1, tensor2
+
+
+class BasicModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features=51204, out_features=5)
+        self.layer_2 = nn.Linear(in_features=5, out_features=1)
+
+    def forward(self, x):
+        return self.layer_2(self.layer_1(x))
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    readDataNx2(FILENAME)
+    X, Y = readDataNx2(FILENAME)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2,  random_state=10)  #create test and train data
+    print(len(X_train), len(X_test), len(Y_train), len(Y_test))
+    Bmodel = BasicModel()
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.SGD(params= Bmodel.parameters(), lr=0.1)
+    Y_logits = Bmodel(X_test.to(device))
+    print(Y_logits)
+
+
+    epochs = 10000
+    for epoch in range(epochs):
+        #training loop
+        Bmodel.train()
+        Y_logits = Bmodel(X_train).squeeze
+        Y_pred = torch.round(torch.sigmoid(Y_logits))
+
+        loss = loss_fn(Y_logits,Y_train)
+        acc = accuracy_fn(y_true=Y_train, y_pred=Y_pred)
+
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        optimizer.step()
+
+        Bmodel.eval()
+
+        #testing loop
+        with torch.inference_mode():
+            test_logits = Bmodel(X_test).squeeze()
+            test_pred = torch.round(torch.sigmoid(test_logits))
+            test_loss = loss_fn(test_logits,Y_test)
+            test_acc = accuracy_fn(Y_test, test_pred)
+
+        if epoch % 10 == 0:
+            print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
