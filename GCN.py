fd88d19af0854d164f3fcd5f846cd095ac4077a7
Grayson Morris <grayson.r.morris@gmail.com>
2023-08-09 14:51:42 -0600

    Started basic testing and training loops

--- GCN.py
+++ GCN.py
@@ -1,5 +1,7 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
 from GC import GraphConvolution
 
 
@@ -16,3 +18,55 @@ class GCN(nn.Module):
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
         return F.log_softmax(x, dim=1)
+    
+# Set up a dataloader here
+
+    
+# Loss Function and Optimizer and accuracy function
+loss_func = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(GCN.parameters(), lr=0.1)
+
+def accuracy_func(output, labels):
+    correct = torch.eq(output, labels).sum().item()
+    acc = correct / len(labels)
+    return acc
+    
+    
+# Set up Training ( basic for now variable and argument names will need to be changed)
+def training(model: torch.nn.Module,
+          loss_func: torch.nn.Module,
+          optimizer: torch.optim.Optimizer,
+          accuracy_func,
+          device: torch.device):
+    model.train()
+    
+    # Put data on target device
+    
+    # Forward pass
+    
+    # Loss and metrics
+    loss = loss_func()
+    train_loss += loss
+    train_acc += accuracy_func()
+    
+    # Rest of loop
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+# Set up Testing ( basic for now variable and argument names will need to be changed)
+
+def testing(model: torch.nn.Module,
+          loss_func: torch.nn.Module,
+          accuracy_func,
+          device: torch.device):
+    model.eval()
+    with torch.inference_mode():
+        # Put data on target device
+    
+        # Forward pass
+    
+        # Loss and metrics
+        loss = loss_func()
+        test_loss += loss
+        test_acc += accuracy_func()
